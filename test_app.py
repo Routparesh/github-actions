edb800908c0cdbae89aa00a405f26ba6190452e6
Paresh Kumar Rout <routpareshkumar737@gmail.com>
2026-07-03 23:04:40 +0530

    update

--- test_app.py
+++ test_app.py
@@ -4,13 +4,13 @@ from app import app
 def test_home_returns_200():
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 200 # nosec
 
 
 def test_health_returns_200():
     client = app.test_client()
     response = client.get('/health')
-    assert response.status_code == 200
+    assert response.status_code == 200 # nosec
 
 
 def test_health_body():
